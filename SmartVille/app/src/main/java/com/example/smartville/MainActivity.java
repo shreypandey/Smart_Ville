75c759eed80afdc3e8d7847864809d75c031b1d0
Shrey Pandey <shreypandey1509@gmail.com>
2019-01-26 03:19:03 +0530

    Login Added

--- SmartVille/app/src/main/java/com/example/smartville/MainActivity.java
+++ SmartVille/app/src/main/java/com/example/smartville/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.smartville;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    static String hasuraurl="https://smart-ville.herokuapp.com/v1alpha1/graphql";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
     }
 }
