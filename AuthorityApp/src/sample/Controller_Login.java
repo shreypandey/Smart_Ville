545d31ee2a58b10dd10857edaf0da477f8ea48d7
suraj-goel <surajgoel.1225@gmail.com>
2019-01-26 14:28:57 +0530

    Reverse GeoCoding Implemented

--- AuthorityApp/src/sample/Controller_Login.java
+++ AuthorityApp/src/sample/Controller_Login.java
@@ -3,6 +3,8 @@ package sample;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import eu.bitm.NominatimReverseGeocoding.Address;
+import eu.bitm.NominatimReverseGeocoding.NominatimReverseGeocodingJAPI;
 import javafx.event.ActionEvent;
 
 public class Controller_Login {
@@ -14,7 +16,13 @@ public class Controller_Login {
         type.getItems().addAll(Main.types);
     }
     public void onloginclicked(ActionEvent actionEvent) {
+        NominatimReverseGeocodingJAPI nominatim1 = new NominatimReverseGeocodingJAPI(); //create instance with default zoom level (18)
 
+//        NominatimReverseGeocodingJAPI nominatim2 = new NominatimReverseGeocodingJAPI(18); //create instance with given zoom level
+
+        Address address =  nominatim1.getAdress(19.10495377, 73.00523376); //returns Address object for the given position
+        String addressString = ""+ address.getCity()+" "+address.getRoad() + " "+address.getSuburb();
+        System.out.println(addressString);
     }
 
     public void onsubjectclicked(ActionEvent actionEvent) {
--- AuthorityApp/src/sample/Main.java
+++ AuthorityApp/src/sample/Main.java
@@ -1,5 +1,9 @@
 package sample;
+import eu.bitm.NominatimReverseGeocoding.Address;
+import eu.bitm.NominatimReverseGeocoding.NominatimReverseGeocodingJAPI;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.web.WebEngine;
@@ -11,21 +15,22 @@ import java.util.List;
 
 public class Main extends Application {
 
-    @Override public void start(Stage stage) {
-        // create web engine and view
-        final WebEngine webEngine = new WebEngine(getClass().getResource("GoogleMap.html").toString());
-        final WebView webView =new WebView();
-        stage.setTitle("Web Map");
-        Scene scene = new Scene(webView,1000,700, Color.web("#666970"));
-        stage.setScene(scene);
-        // show stage
-        stage.show();
+    @Override public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Authority-App");
+        primaryStage.setScene(new Scene(root, 1081, 826));
+//        primaryStage.setMaximized(true);
+        primaryStage.show();
+
         types.add(("LostAndFound"));
         types.add("Police");
         types.add("HealthCare");
         types.add("FireSafety");
         types.add("Traffic");
         types.add("Organising");
+
+
     }
 
     static
